ec874e62ba6eee9ba2ef531eaf1af36ab50818d3
newmoon93 <seongwon93@gmail.com>
2023-02-04 22:42:08 +0900

    clean

--- .ccls-cache/@home@runner@CPPStudy/study.h
+++
@@ -1,7 +0,0 @@
-int study_48p(void);
-int study_69p(void);
-int study_73p(void);
-int study_75p(void);
-int doodle_cpp_65(void);
-int study_91p(void);
-int doodle_cpp_50(void);
--- study_91p.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "study.h"
-
-using namespace std;
-
-char * 
